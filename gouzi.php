1ac58ef8fb2dbacdeb28e225d914bcc3000570a4
Feiyuan <1173691105@qq.com>
2020-03-18 11:23:11 +0800

    狗子

---
+++ gouzi.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <script src="./js/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="./css/homepage.css" />
+</head>
+<body>
+ <img src="pic/gouzi.JPG" height="700" width="700"><h2>爱你哦~</h2>
+</body>
+</html>
+
